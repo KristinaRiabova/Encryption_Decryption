12948e267ede8b364e3d223481636036557eacac
KristinaRiabova <kristinaer304@gmail.com>
2023-11-07 19:31:03 +0200

    encrypt and decrypt files

--- main.py
+++ main.py
@@ -13,3 +13,21 @@ def caesar_cipher(text, shift, encrypt=True):
     else:
         shift = -shift
         return ''.join(map(shift_char, text))
+
+def encrypt_file(input_file, output_file, shift):
+    with open(input_file, 'r') as f:
+        raw_text = f.read()
+
+    encrypted_text = caesar_cipher(raw_text, shift)
+
+    with open(output_file, 'w') as f:
+        f.write(encrypted_text)
+
+def decrypt_file(input_file, output_file, shift):
+    with open(input_file, 'r') as f:
+        encrypted_text = f.read()
+
+    decrypted_text = caesar_cipher(encrypted_text, shift, encrypt=False)
+
+    with open(output_file, 'w') as f:
+        f.write(decrypted_text)
