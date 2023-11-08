bfc831dac7ea6b83f53cc6818f659f23629896f4
KristinaRiabova <kristinaer304@gmail.com>
2023-11-08 07:20:55 +0200

    functional programming principles

--- main.py
+++ main.py
@@ -1,53 +1,53 @@
+
+def encrypt_char(char, key):
+    if char.isupper():
+        return chr((ord(char) - ord('A') + key) % 26 + ord('A'))
+    elif char.islower():
+        return chr((ord(char) - ord('a') + key) % 26 + ord('a'))
+    else:
+        return char
+
+
+def decrypt_char(char, key):
+    if char.isupper():
+        return chr((ord(char) - ord('A') - key) % 26 + ord('A'))
+    elif char.islower():
+        return chr((ord(char) - ord('a') - key) % 26 + ord('a'))
+    else:
+        return char
+
+
+def process_text(text, key, transformation_func):
+    return ''.join(transformation_func(char, key) for char in text)
+
+
 def encrypt(raw_text, key):
-    encrypted_text = []
-
-    for char in raw_text:
-        if char.isupper():
-            encrypted_char = chr((ord(char) - ord('A') + key) % 26 + ord('A'))
-        elif char.islower():
-            encrypted_char = chr((ord(char) - ord('a') + key) % 26 + ord('a'))
-        else:
-            encrypted_char = char
-
-        encrypted_text.append(encrypted_char)
-
-    return ''.join(encrypted_text)
+    return process_text(raw_text, key, encrypt_char)
 
 
 def decrypt(encrypted_text, key):
-    decrypted_text = []
-
-    for char in encrypted_text:
-        if char.isupper():
-            decrypted_char = chr((ord(char) - ord('A') - key) % 26 + ord('A'))
-        elif char.islower():
-            decrypted_char = chr((ord(char) - ord('a') - key) % 26 + ord('a'))
-        else:
-            decrypted_char = char
-
-        decrypted_text.append(decrypted_char)
-
-    return ''.join(decrypted_text)
+    return process_text(encrypted_text, key, decrypt_char)
 
 
-
-def encrypt_file(input_file, output_file, key):
+def encrypt_file(input_file, key):
     with open(input_file, 'r') as f:
         raw_text = f.read()
 
-    encrypted_text = encrypt(raw_text, key)
+    return encrypt(raw_text, key)
 
-    with open(output_file, 'w') as f:
-        f.write(encrypted_text)
 
-def decrypt_file(input_file, output_file, key):
+
+def decrypt_file(input_file, key):
     with open(input_file, 'r') as f:
         encrypted_text = f.read()
 
-    decrypted_text = decrypt(encrypted_text, key)
+    return decrypt(encrypted_text, key)
 
+
+
+def write_text_to_file(text, output_file):
     with open(output_file, 'w') as f:
-        f.write(decrypted_text)
+        f.write(text)
 
 def main():
     while True:
@@ -59,13 +59,15 @@ def main():
             input_file = input("Enter the input file name: ")
             output_file = input("Enter the output file name: ")
             key = int(input("Enter the encryption key: "))
-            encrypt_file(input_file, output_file, key)
+            encrypted_text = encrypt_file(input_file, key)
+            write_text_to_file(encrypted_text, output_file)
             print("Encryption complete.")
         elif choice == 'D':
             input_file = input("Enter the input file name: ")
             output_file = input("Enter the output file name: ")
             key = int(input("Enter the decryption key: "))
-            decrypt_file(input_file, output_file, key)
+            decrypted_text = decrypt_file(input_file, key)
+            write_text_to_file(decrypted_text, output_file)
             print("Decryption complete.")
         else:
             print("Invalid choice. Please enter 'E', 'D', or 'exit'.")
